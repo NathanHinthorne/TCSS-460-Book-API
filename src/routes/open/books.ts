cf32693a769bbf7d8fbfd49f99ca0e0521a8b0ec
Nathan Hinthorne <nathanrh856@gmail.com>
2024-05-06 12:51:40 -0700

    updated api documentation

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -64,56 +64,101 @@ const bookRouter: Router = express.Router();
 ==============================================================
 */
 
+/*
+NOTE: We might want to return book objects instead of
+      strings which describe the books.
+      This would be more useful for the front end
+      since we wouldn't have to spend time parsing the strings.
+
+doing this would look like:
+
+ @apiSuccess {Object} book The book
+ @apiSuccess {String} book.isbn13 The ISBN of the book
+ @apiSuccess {String} book.authors The authors of the book
+ @apiSuccess {String} book.publication_year The year the book was published
+ @apiSuccess {String} book.original_title The original title of the book
+ @apiSuccess {String} book.title The title of the book
+ @apiSuccess {String} book.rating_avg The average rating of the book
+ @apiSuccess {String} book.rating_count The number of ratings the book has
+ @apiSuccess {String} book.rating_1_star The number of 1 star ratings the book has
+ @apiSuccess {String} book.rating_2_star The number of 2 star ratings the book has
+ @apiSuccess {String} book.rating_3_star The number of 3 star ratings the book has
+ @apiSuccess {String} book.rating_4_star The number of 4 star ratings the book has
+ @apiSuccess {String} book.rating_5_star The number of 5 star ratings the book has
+ @apiSuccess {String} book.image_url The URL of the image of the book
+ @apiSuccess {String} book.image_small_url The URL of the small image of the book
+
+as you can see above, this is a lot of lines of code to write
+*/
+
 
 // ---------------- GET ----------------
 
 /**
- * Get
- * All books (required) - returns all books in the database 
+ * NOTE: This is a required endpoint
  * 
- * @api {get} /allBooks
+ * @api {get} /books Get all books
+ * 
+ * @apiDescription Get all books in the database
+ * 
  * @apiName GetAllBooks
  * @apiGroup User
- * @apiVersion 0.1.0
- * @apiSuccess {Object[]} bookList List of all the books
  * 
- * @apiSuccessExample  Success-Response:
- *  { "title":  "Animal Farm", "titles":  "...” , ect. }
+ * @apiSuccess {String[]} bookList List of all the books as the following string: 
+ *       "Title: {title}, Author: {author}, ISBN: {isbn}"             
+ * @apiSuccessExample {json} Success-Response:
+ * { "bookList":  "Title: Animal Farm, Author: George Orwell, ISBN: 9780451526342" }
+ * 
+ * @apiError (404: Books Not Found) No books were found in the database
  */
 
-// @apiSuccess {String} bookList.title The books title
 
 /** 
- * ISBN (required) - get specific book by ISBN
- * @api {get} /books Get Book by ISBN
+ * NOTE: This is a required endpoint
+ * @api {get} /books/:isbn Get specific book by ISBN
+ * 
+ * @apiDescription Get a specific book by ISBN
+ * 
  * @apiName GetByISBN
  * @apiGroup User
- * @apiVersion 0.1.0
+ * 
  * @apiParam {String} The ISBN paired with a given book
- * @apiSuccess {String} title The books title
- * @apiSuccessExample {json} Success-Response:
- *  { "title":  "Charlotte’s Web" }
+ * 
+ * @apiSuccess {String} book The book as the following string:
+ *      "Title: {title}, Author: {author}, ISBN: {isbn}"
+ * @apiSuccessExample {String} Success-Response:
+ * "book":  "Title: Animal Farm, Author: George Orwell, ISBN: 9780451526342"
+ * 
+ * @apiError (404: Book Not Found) The book with the given ISBN was not found
 */
 
 /**
- * Author (required) - get books by an author
- * @api {get} /author
+ * NOTE: This is a required endpoint
+ * @api {get} /books?author=:author Get books by an author
+ * 
+ * @apiDescription Get books by a given author
+ * 
  * @apiName GetByAuthors
  * @apiGroup User
- * @apiVersion 0.1.0
+ * 
  * @apiQuery {String} author The author's full name
+ * 
  * @apiSuccess {String} titles List of book titles by given author
  * @apiSuccessExample {json} Success-Response:
  *   { "titles":  "Heaven, Ashley Bell, Falling Up" }
  */
 
 /**
- * Rating (required) - get books by a rating 
- * @api {get} /rating
+ * NOTE: This is a required endpoint
+ * @api {get} /books?rating=:rating Get books by a rating 
+ * 
+ * @apiDescription Get books by a given rating
+ * 
  * @apiName GetByRating
  * @apiGroup User
+ * 
  * @apiQuery {Number} rating_avg The rating the books need to be at or above
- * @apiVersion 0.1.0
+ * 
  * @apiSuccess {String} title The books with the given
  * @apiSuccessExample {json} Success-Response:
  *   { "title":  "Animal Farm",
@@ -122,12 +167,15 @@ const bookRouter: Router = express.Router();
  */
 
 /**
- * Title (required) - get books by a title 
- * @api {get} /title
+ * @api {get} /books?title=:title Get books by a title 
+ * 
+ * @apiDescription Get books by a given title
+ * 
  * @apiName GetByTitle
  * @apiGroup User
- * @apiVersion 0.1.0
+ * 
  * @apiQuery {String} title The title of a book
+ * 
  * @apiSuccess {String} title The books title
  * @apiSuccessExample {json} Success-Response:
  *   { "title":  "Animal Farm" }
@@ -135,11 +183,16 @@ const bookRouter: Router = express.Router();
 
 /**
  * Publish year
- * @api {get} /publishYear
+ * @api {get} /books?year=:year Get books by publish year
+ * 
+ * @apiDescription Get books by a given publication year
+ * 
  * @apiName PubYear
  * @apiGroup User
- * @apiVersion 0.1.0
- * @apiSuccess {Number} publication_year The year the book was published 
+ * 
+ * @apiQuery {Number} publication_year The year the book was published
+ * 
+ * @apiSuccess {String} title The book's title
  * @apiSuccessExample {json} Success-Response:
  *   { "publication_year":  "1945" }
  */
@@ -157,8 +210,12 @@ const bookRouter: Router = express.Router();
  * Put
  * Update fields from ISBN
  * @api {put} /ISBN
+ * 
+ * @apiDescription Update fields from a book by ISBN
+ * 
  * @apiName UpdateBookFields
  * @apiGroup Admin
+ * 
  * @apiBody {String} [Book[isbn]]   	  
  * @apiBody {String} [Book[fieldType]]    The field we want to change
  * @apiBody {String} [Book[data]]   	  The new data we want to put in the field
@@ -166,36 +223,55 @@ const bookRouter: Router = express.Router();
 
 /**
  * Rating 1
- * In the back end, update the average rating
+ * NOTE: In the back end, update the average rating
  * @api {put} /rating1
+ * 
+ * @apiDescription Update the number of 1 star ratings for a book
+ * 
  * @apiName UpdateRating1
  * @apiGroup User
  */
 
 /**
  * Rating 2
+ * NOTE: In the back end, update the average rating
  * @api {put} /rating2
+ * 
+ * @apiDescription Update the number of 2 star ratings for a book
+ * 
  * @apiName UpdateRating2
  * @apiGroup User
  */
 
 /**
  * Rating 3
+ * NOTE: In the back end, update the average rating
  * @api {put} /rating3
+ * 
+ * @apiDescription Update the number of 3 star ratings for a book
+ * 
  * @apiName UpdateRating3
  * @apiGroup User
  */
 
 /**
  * Rating 4
+ * NOTE: In the back end, update the average rating
  * @api {put} /rating4
+ * 
+ * @apiDescription Update the number of 4 star ratings for a book
+ * 
  * @apiName UpdateRating4
  * @apiGroup User
  */
 
 /**
  * Rating 5
+ * NOTE: In the back end, update the average rating
  * @api {put} /rating5
+ * 
+ * @apiDescription Update the number of 5 star ratings for a book
+ * 
  * @apiName UpdateRating5
  * @apiGroup User
 */
@@ -205,9 +281,13 @@ const bookRouter: Router = express.Router();
  * Put:
  * Title
  * @api {put} /title
+ * 
+ * @apiDescription Update the title of a book
+ * 
  * @apiName UpdateTitle
  * @apiGroup Admin
- * @apiSuccess  {String} title The books title
+ * 
+ * @apiSuccess {String} title The books title
  * @apiSuccessExample Success-Response:
     { "title":  "Animal Farm" }
  */
@@ -215,19 +295,18 @@ const bookRouter: Router = express.Router();
 /**
  * Author
  * @api {put} /author
+ * 
+ * @apiDescription Update the author of a book
+ * 
  * @apiName UpdateAuthor6
  * @apiGroup Admin
- * @apiVersion 0.1.0
+ * 
  * @apiQuery {String} title The title of the book to be updated 
+ * 
  * @apiSuccess {String} title
  * @apiSuccessExample {json} Success-Response:
  *   { "titles":  "..." }
 */
-
-// (average rating will be updated on the backend whenever rating 1/2/3/4/5 is updated)
-
-
-
 
 
 
@@ -238,6 +317,9 @@ const bookRouter: Router = express.Router();
  * Post
  * One post, fill in from body section
  * @api {post} / addBook
+ * 
+ * @apiDescription Add a book to the database
+ * 
  * @apiName AddBook
  * @apiGroup Admin
 */
@@ -254,8 +336,12 @@ const bookRouter: Router = express.Router();
  * Delete
  * ISBNs 1 or more (required)
  * @api {delete} / ISBN
+ * 
+ * @apiDescription Delete one or more books by ISBN
+ * 
  * @apiName DeleteISBNs
  * @apiGroup Admin
+ * 
  * @apiBody {String[]} [isbn]   array of ISBNs. Can be 1 or more
  */
 
