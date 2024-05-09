b07e400bcaa546612f2200ec105029d80d19474d
Jenna Timm <jennatimm@hotmail.com>
2024-05-08 19:09:57 -0700

    Got one entry point working for books

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -20,8 +20,6 @@ This file can be split into 3 pieces:
 
 */
 
-
-
 /*
 ==============================================================
 1. Setup
@@ -37,8 +35,6 @@ import { pool, validationFunctions } from '../../core/utilities';
 
 const bookRouter: Router = express.Router();
 
-
-
 /*
 ==============================================================
 2. Middlewear functions
@@ -51,11 +47,6 @@ const bookRouter: Router = express.Router();
 ==============================================================
 */
 
-
-
-
-
-
 /*
 ==============================================================
 3. Endpoint handlers
@@ -65,11 +56,11 @@ const bookRouter: Router = express.Router();
 */
 
 /**
- * This defines what a "book" object looks like. 
+ * This defines what a "book" object looks like.
  * It keeps us from typing the book object out multiple times.
- * 
+ *
  * This particular one is for whenever we need to return a book object.
- * 
+ *
  * @apiDefine BookSuccess
  * @apiSuccess {String} book.isbn13 The ISBN of the book
  * @apiSuccess {String} book.authors The authors of the book
@@ -88,11 +79,11 @@ const bookRouter: Router = express.Router();
  */
 
 /**
- * This defines what a "book" object looks like. 
+ * This defines what a "book" object looks like.
  * It keeps us from typing the book object out multiple times.
- * 
+ *
  * This particular one is for whenever we need a book object as input in the body.
- * 
+ *
  * @apiDefine BookBody
  * @apiBody {String} book.isbn13 The ISBN of the book
  * @apiBody {String} book.authors The authors of the book
@@ -110,25 +101,23 @@ const bookRouter: Router = express.Router();
  * @apiBody {String} book.image_small_url The URL of the small image of the book
  */
 
-
-
 // ---------------- GET ----------------
 
 /**
  * NOTE: This is a required endpoint
- * 
+ *
  * @api {get} /books Get all books
- * 
+ *
  * @apiDescription Get all books in the database
- * 
+ *
  * @apiName GetAllBooks
  * @apiGroup User
- * 
+ *
  * @apiSuccess {Object[]} bookList The list of books in the database
  * @apiUse BookSuccess
- * 
+ *
  * @apiSuccessExample {json} Success-Response:
- * { 
+ * {
  *   "bookList":  [
  *     {
  *       "isbn13": "9780451526342",
@@ -148,25 +137,48 @@ const bookRouter: Router = express.Router();
  *     }
  *   ]
  * }
- * 
+ *
  * @apiError (404: Books Not Found) {String} message No books were found in the database
  */
+bookRouter.get('/all', (request: Request, response: Response) => {
+    const theQuery = 'SELECT * FROM BOOKS';
 
+    pool.query(theQuery)
+        .then((result) => {
+            if (result.rowCount >= 1) {
+                response.send({
+                    books: result.rows,
+                });
+            } else {
+                response.status(404).send({
+                    message: 'Books not found',
+                });
+            }
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET all');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
+});
 
-/** 
+/**
  * NOTE: This is a required endpoint
  * @api {get} /books/:isbn Get book by ISBN
- * 
+ *
  * @apiDescription Get a specific book by ISBN
- * 
+ *
  * @apiName GetByISBN
  * @apiGroup User
- * 
+ *
  * @apiParam {String} isbn The ISBN paired with a given book
- * 
+ *
  * @apiSuccess {Object} book The book with the given ISBN
  * @apiUse BookSuccess
- * 
+ *
  * @apiSuccessExample {json} Success-Response:
  * {
  *   "book": {
@@ -186,29 +198,28 @@ const bookRouter: Router = express.Router();
  *       "image_small_url": "http://example.com/small_image.jpg"
  *    }
  * }
- * 
- * 
+ *
+ *
  * @apiError (400: Bad Request) {String} message The requested ISBN is not valid
-*/
-
+ */
 
 /**
  * NOTE: This is a required endpoint
  * @api {get} /books?author=:author Get books by author
- * 
+ *
  * @apiDescription Get books by a given author
- * 
+ *
  * @apiName GetByAuthors
  * @apiGroup User
- * 
+ *
  * @apiQuery {String} author The author's full name
- * 
+ *
  * @apiSuccess {Object[]} bookList The list of books by the given author
  * @apiUse BookSuccess
- * 
+ *
  * @apiSuccess {String} titles List of book titles by given author
  * @apiSuccessExample {json} Success-Response:
- * { 
+ * {
  *   "bookList":  [
  *     {
  *       "isbn13": "9780451526342",
@@ -228,27 +239,26 @@ const bookRouter: Router = express.Router();
  *     }
  *   ]
  * }
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided author is not valid or supported
  */
 
-
 /**
  * NOTE: This is a required endpoint
- * @api {get} /books?rating=:rating Get books by rating 
- * 
+ * @api {get} /books?rating=:rating Get books by rating
+ *
  * @apiDescription Get books by a given rating
- * 
+ *
  * @apiName GetByRating
  * @apiGroup User
- * 
+ *
  * @apiQuery {Number} rating_avg The rating the books need to be at or above
- * 
+ *
  * @apiSuccess {Object[]} bookList The list of books with the given rating or above
  * @apiUse BookSuccess
- * 
+ *
  * @apiSuccessExample {json} Success-Response:
- * { 
+ * {
  *   "bookList":  [
  *     {
  *       "isbn13": "9780451526342",
@@ -268,26 +278,25 @@ const bookRouter: Router = express.Router();
  *     }
  *   ]
  * }
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided rating is not valid or supported
  */
 
-
 /**
- * @api {get} /books?title=:title Get books by title 
- * 
+ * @api {get} /books?title=:title Get books by title
+ *
  * @apiDescription Get books by a given title
- * 
+ *
  * @apiName GetByTitle
  * @apiGroup User
- * 
+ *
  * @apiQuery {String} title The title of the book
- * 
+ *
  * @apiSuccess {Object[]} bookList The list of books with the given title
  * @apiUse BookSuccess
- * 
+ *
  * @apiSuccessExample {json} Success-Response:
- * { 
+ * {
  *   "bookList":  [
  *     {
  *       "isbn13": "9780451526342",
@@ -307,27 +316,26 @@ const bookRouter: Router = express.Router();
  *     }
  *   ]
  * }
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided title is not valid or supported
  */
-
 
 /**
  * Publish year
  * @api {get} /books?year=:year Get books by publication year
- * 
+ *
  * @apiDescription Get books by a given publication year
- * 
+ *
  * @apiName PubYear
  * @apiGroup User
- * 
+ *
  * @apiQuery {Number} publication_year The year the book was published
- * 
+ *
  * @apiSuccess {Object[]} bookList The list of books with the given publication year
  * @apiUse BookSuccess
- * 
+ *
  * @apiSuccessExample {json} Success-Response:
- * { 
+ * {
  *   "bookList":  [
  *     {
  *       "isbn13": "9780451526342",
@@ -347,54 +355,49 @@ const bookRouter: Router = express.Router();
  *     }
  *   ]
  * }
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided publication year is not valid or supported
  */
-
-
-
-
 
 // ---------------- PUT ----------------
 
 /**
  * @api {put} /books/:isbn Update book fields by ISBN
- * 
+ *
  * @apiDescription Update specific fields of a book identified by its ISBN
- * 
+ *
  * @apiName UpdateBookFields
  * @apiGroup Admin
- * 
+ *
  * @apiParam {String} isbn The ISBN of the book to be updated
- * 
+ *
  * @apiBody {Object} fields The fields to be updated with their new values
  * @apiUse BookBody
- * 
+ *
  * @apiSuccess {Object} book The updated book
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided ISBN or fields are not valid
  * @apiError (404: Not Found) {String} message The book with the provided ISBN was not found
  */
 
-
 /**
  * NOTE: In the back end, update the average rating and rating count since the rating has changed
- * NOTE: Since a user can only rate a book once, the rating count should only ever increase by 1. 
+ * NOTE: Since a user can only rate a book once, the rating count should only ever increase by 1.
  *      If a user changes their rating, the old rating should be removed and the new rating should be added.
- * 
+ *
  * @api {put} /books/:isbn/rating/:rating Increment book rating
- * 
+ *
  * @apiDescription Increment the ratings of a book. The rating type should be between 1 and 5.
- * 
+ *
  * @apiName IncrementRating
  * @apiGroup User
- * 
+ *
  * @apiParam {String} isbn The ISBN of the book to be updated
  * @apiParam {Number} rating The rating type to be updated (1-5)
- * 
+ *
  * @apiSuccess {Object} book The updated book
  * @apiUse BookSuccess
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided ISBN, rating, or count are not valid
  * @apiError (404: Not Found) {String} message The book with the provided ISBN was not found
  */
@@ -402,26 +405,25 @@ const bookRouter: Router = express.Router();
 /**
  * NOTE: In the back end, update the average rating and rating count since the rating has changed
  * NOTE: An admin is allowed to adjust the ratings without a limit.
- * 
+ *
  * @api {put} /books/:isbn/rating/:rating Update book rating
- * 
+ *
  * @apiDescription Update the ratings of a book. The rating type should be between 1 and 5.
- * 
+ *
  * @apiName UpdateRating
  * @apiGroup Admin
- * 
+ *
  * @apiParam {String} isbn The ISBN of the book to be updated
  * @apiParam {Number} rating The rating type to be updated (1-5)
- * 
+ *
  * @apiBody {Number} count The new count for the specified rating
- * 
+ *
  * @apiSuccess {Object} book The updated book
  * @apiUse BookSuccess
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided ISBN, rating, or count are not valid
  * @apiError (404: Not Found) {String} message The book with the provided ISBN was not found
  */
-
 
 // I haven't finished these two endpoints yet. -Nathan
 /*
@@ -442,38 +444,35 @@ const bookRouter: Router = express.Router();
 /*
  * Author
  * @api {put} /author
- * 
+ *
  * @apiDescription Update the author of a book
- * 
+ *
  * @apiName UpdateAuthor6
  * @apiGroup Admin
- * 
- * @apiQuery {String} title The title of the book to be updated 
- * 
+ *
+ * @apiQuery {String} title The title of the book to be updated
+ *
  * @apiSuccess {String} title
  * @apiSuccessExample {json} Success-Response:
  *   { "titles":  "..." }
-*/
-
-
-
+ */
 
 // ---------------- POST ----------------
 
 /**
  * NOTE: Required endpoint
  * NOTE: This endpoint should allow null values for fields that are not required
- * 
+ *
  * @api {post} /books Add a new book
- * 
+ *
  * @apiDescription Add a new book to the database
- * 
+ *
  * @apiName AddBook
  * @apiGroup Admin
- * 
+ *
  * @apiBody {Object} book The book to be added
  * @apiUse BookBody
- * 
+ *
  * @apiParamExample {json} Request-Example:
  * {
  *   "isbn13": "9780451526342",
@@ -491,18 +490,12 @@ const bookRouter: Router = express.Router();
  *   "image_url": "http://example.com/image.jpg",
  *   "image_small_url": "http://example.com/small_image.jpg"
  * }
- * 
+ *
  * @apiSuccess {Object} book The book that was added
  * @apiUse BookSuccess
- * 
+ *
  * @apiError (400: Bad Request) {String} message The provided book data is not valid
-*/
-
-
-
-
-
-
+ */
 
 // ---------------- DELETE ----------------
 
@@ -510,17 +503,17 @@ const bookRouter: Router = express.Router();
  * NOTE: Required endpoint
  * ISBNs 1 or more (required)
  * @api {delete} /isbn Delete books by ISBN
- * 
+ *
  * @apiDescription Delete one or more books by ISBN
- * 
+ *
  * @apiName DeleteISBNs
  * @apiGroup Admin
- * 
+ *
  * @apiBody {String[]} isbns array of ISBNs. Can be 1 or more
- * 
+ *
  * @apiSuccess {String[]} isbns The ISBNs of the books that were deleted
- * 
+ *
  * @apiError (400: Bad Request) {String} message At least one of the provided ISBNs is not valid
  */
 
-
+export { bookRouter };
